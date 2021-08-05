b2ae77cc3c0ffcd645868767d6c29e7545f63840
VibhutiSingh12 <69121253+vibhuti647@users.noreply.github.com>
2021-08-05 17:16:24 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,45 @@
+print("Systematic investment plan")
+print("==========================\n")
+
+p = input("enter your principal:")
+print("---------------------------")
+r = input("enter your rate:")
+print("---------------------------")
+t = input("enter your time:")
+print("------------------------")
+
+monthly_amount = int(p)*(1+int(r)/100)**int(t)
+SIP_amount = int(p)*((1+int(r)/100/12)**int(t)-1)/int(r)/100/12*(1+int(r)/100/12)
+ 
+ 
+ 
+
+print("--------------------------------------")
+print("your monthly amount:",monthly_amount,)
+print("--------------------------------------\n\n")
+print("-----------------------------")
+ 
+
+ 
+
+
+
+ 
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
